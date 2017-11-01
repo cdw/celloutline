3a7c49ee71b33703c52742e913b0105280c893c3
Dave Williams <cdave@uw.edu>
2017-11-01 11:19:53 -0700

    Continue refactoring conversions out of representations

--- celloutline/representation.py
+++ celloutline/representation.py
@@ -17,19 +17,45 @@ __all__ = ["BinaryVoxel", "SpreadVoxel",
 """ Comparisons between the representations; happen at mesh level """
 
 
+def mesh_error(mesh1, mesh2):
+    """Error (intersection over union) of the two meshes"""
+    intersection = mesh1.intersection(mesh2)
+    union = mesh1.union(mesh2)
+    mesh_error = intersection.volume/union.volume
+    return mesh_error
 
 
+""" Representational classes """
+ 
 
 class Representation:
-    """Do we want a base class?"""
-    def __init__(self):
-        return
+    """What all the forms of a segmentation are based on"""
+    def __init__(self, id, source=None):
+        """Remember where we came from
+        
+        Parameters
+        ----------
+        id: string
+            key or filename
+        source: collection or None
+            dict or other collection for key, None if the id 
+            is a filename
+        """
+        self.id = id
+        self.source = source
+        self._voxels = None
+        self._spread = None
+        self._spiral = None
+        self._mesh = None
+    
+    def error(self, other):
+        """Mismatch between this and another representation"""
+        return mesh_error(self.mesh, other.mesh)
 
 
-
-class BinaryVoxel:
-    """Reading and comparison of binary cell segmentations"""
-    def __init__(self, input_fn, voxels=None):
+class BinaryVoxel(Representation):
+    """Reading and conversion of binary cell segmentations"""
+    def __init__(self, id, source=None, voxels=None):
         """This is the native representation of 3D microscopy data
 
         We read in the binary voxel representation from numpy arrays 
@@ -38,165 +64,145 @@ class BinaryVoxel:
 
         Parameters
         ----------
-        input_fn: string
-            path to underlying voxel file
+        id: string
+            key or filename
+        source: collection or None
+            dict or other collection for key, None if the id 
+            is a filename
+        voxels: i-by-j-by-k array or None
+            the voxels of interest or nothing (in which case we load
+            them on request)
         """
-        self.fn = input_fn 
+        super().__init__(id, source)
         self._voxels = voxels # lazy load initial data
-        self._spread = None   # and don't create downstream
-        self._spiral = None   # data until it is needed for 
-        self._mesh = None     # reduced memory consumption
-        # Cache results when possible, keep track
-        vox_recalc = voxels is None # needed if not passed
-        self.needs_recalc = {'voxels': vox_recalc,
-                             'spread': True, 
-                             'spiral': True,
-                             'mesh': True}
-        return 
 
     @property
     def voxels(self):
         """The binary voxel representation of the cell"""
-        if self.needs_recalc['voxels'] is True:
-            self._voxels = np.load(self.fn)['cell']
-            self.needs_recalc['voxels'] = False
+        if self._voxels is None:
+            if self.source is None: # load from disc
+                self._voxels = np.load(self.id)['cell']
+            else: # load from source collection
+                self._voxels = self.source[self.id]
         return self._voxels
 
     @property
-    def spread(self, options={}):
-        """A spread representation of the binary voxels"""
-        if 
-        sl
-        return SpreadVoxel
-
-    def mismatch(self, recon):
-        orig = self.read()
-        size = orig.size
-        assert orig.shape==recon.shape, "shapes don't match"
-        absdiff = np.sum(np.abs(recon-orig))
-        normed = absdiff/size
-        return normed
-
-    def spiral(self, spiral, origin, options={}):
-        """ Convert binary image to radius, phi, theta spiral
-
-        Parameters
-        ----------
-        spiral: Spiral class
-            spiral.rpt are the rays we will trace along to find 
-            intersections with the shell of the segmentation
-        origin: 3 length vector
-            x,y,z origin of the segmentation from which the rays emerge
-        options: dict, optional
-            
-        Returns
-        -------
-        spiral: SpiralizedTrace class instance
-            instance of a spiralized trace
-
-        """
-        shell = segmentation.find_boundaries(cell, connectivity=1, mode='outer')
-        surf_coords = list(np.array(shell.nonzero()).T)
-        origin = np.divide(shell.shape,2).astype(int)
-        output_pts = []
-        for ray in sample_pts:
-            output_pts.append(nearest_intersecting((origin, ray), surf_coords))
-        radii = [dist(origin, pt, 0.5) for pt in output_pts]
-        return output_pts, radii
-        
-    def mesh(self, options={}):
-        """ Convert binary image to a mesh
-
-        Parameters
-        ----------
-        options: dict, optional
-            Marching cubes control options, documented in skimage_. 
-            Notable settings include, ``level``, a float level to 
-            draw the boundary at.
-        
-        Returns
-        -------
-        mesh: Mesh class instance
-            Marching cubes meshed output
-
-        .. _skimage: http://scikit-image.org/docs/dev/api/skimage.measure.html#marching-cubes-lewiner
-        """
-        verts, faces, _, _ = marching_cubes(self.binary, **options)
-        tm = trimesh.Trimesh(verts, faces)
-        return Mesh(tm)
-
-
-
-class SpiralizedTrace:
-    """Reading and comparison of cell surface traces"""
-    def __init__(self, inobj, rays_rpt, origin):
-        """inobj must be an array or support a read method"""
-        self.inobj = inobj
-        self.rays_rpt = rays_rpt
-        self.origin = origin
-
-    @property
-    def trace(self):
-        if type(self.inobj)==np.array:
-            return self.inobj
-        else:
-            return self.inobj.read()
-
-    @property
-    def point_cloud(self):
-        # rpt = np.hstack((np.expand_dims(self.trace, -1), self.rays_rpt[:,1:]))
-        # xyz = sphere_to_cart(rpt)
-        # xyz += self.origin
-        #return xyz
-        return sprial_to_point_cloud(self.trace, self.rays_rpt, self.origin)
-
-    def mismatch(self, shell):
-        shell_locs = np.array(np.nonzero(shell)).T
-        pt_cloud = self.point_cloud
-        dists = [np.min(spdist.cdist(pt[np.newaxis,:], shell_locs)) for pt in pt_cloud]
-        return np.mean(dists)
-        pass
-
-
-
-class SpreadVoxels:
-    """SpreadVoxels is a derived structure from a set of binary voxels"""
-    def __init__(self, ):
-        self.parent = parent_binary_voxels
-        self.needs_recalc = {'spread': True,
-                             'mesh': True}
-        return
+    def mesh(self):
+        """The mesh of the voxels"""
+        if self._mesh is None:
+            self._mesh = conversions.binary_to_mesh(self.voxels)
+            assert self._mesh.is_watertight, "leaky mesh"
+        return self._mesh
 
     @property
     def spread(self):
-        """Calculate the level-set for the parent binary voxels"""
-        if self.needs_recalc['spread']:
-            vox = self.parent.voxels
-            neg_interior = -scipy.ndimage.distance_transform_edt(vox)
-            pos_exterior = scipy.ndimage.distance_transform_edt(1-vox)
-            self._spread = pos_exterior + neg_interior
-            self.needs_recalc['spread'] = False
-    
-    def mesh_error(self, mesh, 
+        """A spread representation of the binary voxels"""
+        if self._spread is None:
+            spread_voxels = conversions.binary_to_spread(self.voxels)
+            self._spread = SpreadVoxels(self.id, 
+                                        self.source, 
+                                        spread_voxels)
+        return self._spread
+
+    def spiral(self, unitspiral=None, num_pts=500):
+        """A spiral representation of the cell"""
+        if self._spiral is None:
+            spidict = conversions.binary_to_spiral(
+                self.voxels, unitspiral, num_pts)
+            self._spiral = SpiralizedTrace(
+                self.id, self.source, **spidict)
+        return self._spiral
+        
+
+class SpiralizedTrace(Representation):
+    """Spiral trace along the cell surface"""
+    def __init__(self, id, source, radii, unitspiral, origin):
+        """ Contain a representation of a spiralized trace and 
+        the unit spiral that is needed to generate it. 
+        
+        Parameters
+        ----------
+        id: string
+            key or filename
+        source: collection or None
+            dict or other collection for key, None if the id 
+            is a filename
+        radii: 1-by-n array
+            list of distances from the origin to the first shell 
+            intersection for a given spiral
+        unitspiral: UnitSpiral object
+            contains unit rays (in same order as radii) with angles
+        origin: 3-by-1 array
+            xyz offset of the center of the segmentation
+        """
+        super().__init__(id, source)
+        self._point_cloud = None
+        self.spiral_dict = {'unitspiral': unitspiral,
+                            'radii': radii,
+                            'origin': origin}
+
     @property
-    def mesh:
-    def read_and_transform(self, fn):
-        pass
-    def _write(self, output_dir):
-        pass
-    @staticmethod
-    def mismatch(original, recon):
-        pass
-    def mismatch_all(self, recons):
-        pass
+    def unitspiral(self):
+        return self.spiral_dict['unitspiral']
+
+    @property
+    def radii(self):
+        return self.spiral_dict['radii']
+
+    @property
+    def origin(self):
+        return self.spiral_dict['origin']
+
+    @property
+    def point_cloud(self):
+        if self._point_cloud is None:
+            self._point_cloud = conversions.spiral_to_point_cloud(
+                **self.spiral_dict)
+        return self._point_cloud
+
+    @property
+    def mesh(self):
+        if self._mesh is None:
+            self._mesh = conversions.spiral_to_mesh(**self.spiral_dict)
+        return self._mesh
 
 
-class Mesh:
-    def __init__(self, input_tm):
-        self.mesh = input_tm
+class SpreadVoxels(Representation):
+    """Level set derived from binary voxels"""
+    def __init__(self, id, source, spread, cutoff=0.0):
+        """
+        Parameters
+        ----------
+        id: string
+            key or filename
+        source: collection or None
+            dict or other collection for key, None if the id 
+            is a filename
+        spread: i-by-j-by-k array
+            the spread voxels of interest 
+        cutoff: float
+            where we set the isosurface for the mesh conversion
+        """
+        super().__init__(id, source)
+        self._spread = spread 
+        self._cutoff = cutoff
+        self._cutoff_change = False
 
-    def union(self, tm):
-        return trimesh.union(self.mesh, tm)
+    @property
+    def cutoff(self):
+        return self._cutoff
 
-    def intersection(self, tm):
-        return trimesh.intersection(self.mesh, tm)
+    @cutoff.setter
+    def cutoff(self, newcutoff):
+        """Set mesh conversion cutoff and trigger new mesh"""
+        self._cutoff = newcutoff
+        self._cutoff_change = True
+
+    @property
+    def mesh(self):
+        """New mesh if not done or if cuttoff has changed"""
+        if self._mesh is None or self._cutoff_change:
+            self._mesh = conversions.spread_to_mesh(
+                self._spread, self._cutoff)
+            self._cutoff_change = False
+        return self._mesh
