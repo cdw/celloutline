35d4690233dcc48dd0adc9d032e3f04bb4073bc8
Dave Williams <cdave@uw.edu>
2018-03-22 15:26:46 -0700

    PEP8 bits

--- celloutline/model.py
+++ celloutline/model.py
@@ -4,23 +4,20 @@ Author: CDW
 """
 
 # Standard or installed
-import os
-import copy
 import pickle
 import numpy as np
 from sklearn.decomposition import IncrementalPCA
-from tqdm import tqdm
 # Local
 from . import representation
 
-
-__all__ = ["PCA", "ICA", "Fourier", "Zernicke", "Autoencoder"]
+__all__ = ["PCA", "ICA", "Fourier", "Autoencoder"]
 
 
 class Model:
-    """Model is a base class that provides some "don't repeat yourself" 
+    """Model is a base class that provides some "don't repeat yourself"
     for models which inherit from it.
     """
+
     def __init__(self, existing_fn=None):
         pass
 
@@ -39,22 +36,23 @@ class Model:
         elif not fn.endswith('.pkl'):
             fn += '.pkl'
         with open(fn, 'wb') as modelfile:
-            pickle.dump(self.model)
-    
+            pickle.dump(self.model, modelfile)
+
     def set_params(self, params=None):
         """Set parameters from passed or local"""
-        if parameters is not None:
-            self.model.set_params(params)
+        if params is not None:
+            self.model.set_params(**params)
         else:
-            self.model.set_params(self.params)
+            self.model.set_params(**self.params)
+
 
 class PCA(Model):
     """Given a set of input vectors, find their principle components"""
+
     def __init__(self, fn=None, n_comp=None, batch_size=None):
         self.model = IncrementalPCA()
         self.fn = fn
-        self.params = {"n_components": n_comp, 
-                       "batch_size": batch_size}
+        self.params = {"n_components": n_comp, "batch_size": batch_size}
         self.set_params()
 
     def load(self, fn):
@@ -96,24 +94,20 @@ class ICA:
     def fit(self):
         pass
 
-    def transform(self, original):
-
-    def inverse_transform(self, transformed):
-
-
     def error(self, original, recon):
         pass
+
 
 class Fourier:
     def __init__(self, reduced_dimensions):
         """Reduce/replicate an image using its Fourier bases
-        
+
         Parameters
         ----------
         reduced_dimensions: int
             number of dimensions to keep when encoding,
             is coerced into being an even cube if needed.
-            Even cubes are (2*n)**3, e.g. 8, 64, 216, 512, 
+            Even cubes are (2*n)**3, e.g. 8, 64, 216, 512,
             1000, 1728, 2744, 4096, 5832, 8000, ...
         """
         self.reduced_dimensions = reduced_dimensions
@@ -127,26 +121,25 @@ class Fourier:
     def reduced_dimensions(self, dims):
         """Enforce even cube, see __init__ for documentation"""
         # Coerce reduced dimensions into even cube form
-        to_keep = round(np.divide(np.power(dims, 1/3), 2))
+        to_keep = round(np.divide(np.power(dims, 1 / 3), 2))
         cubed_dims = np.power(np.multiply(to_keep, 2), 3).astype(int)
         self._reduced_dimensions = cubed_dims
         # Give a warning if the coercion changed the number of dims
         if cubed_dims != dims:
             import warnings
             warnings.warn("Passed dimensions, %i, not an even cube. \
-                          Converted to %i"%(reduced_dimensions, cubed_dims))
+                          Converted to %i" % (reduced_dimensions, cubed_dims))
         return
 
     @property
     def _mask(self):
         """Derive mask from original shape"""
         center = np.divide(self.original_shape, 2).astype(int)
-        to_keep = int(round(
-            np.divide(np.power(self.reduced_dimensions, 1/3), 2)))
+        to_keep = int(
+            round(np.divide(np.power(self.reduced_dimensions, 1 / 3), 2)))
         mask = np.zeros(self.original_shape)
-        mask[center[0]-to_keep : center[0]+to_keep, 
-             center[1]-to_keep : center[1]+to_keep,
-             center[2]-to_keep : center[2]+to_keep] = 1
+        mask[center[0] - to_keep:center[0] + to_keep, center[1] - to_keep:
+             center[1] + to_keep, center[2] - to_keep:center[2] + to_keep] = 1
         return mask
 
     @property
@@ -161,23 +154,23 @@ class Fourier:
 
     def encoded_to_flat(self, encoded):
         """Flatten the encoded representation"""
-        assert encoded.shape==self.original_shape, "Shape mismatch, reset"
+        assert encoded.shape == self.original_shape, "Shape mismatch, reset"
         selected_freqs = np.fft.fftshift(encoded)[self._mask.astype(bool)]
         return selected_freqs.flatten()
 
     def flat_to_encoded(self, flat):
         """Return flat representation to encoded dimensions for decoding"""
-        cube_side = int(round(np.power(self.reduced_dimensions, 1/3)))
+        cube_side = int(round(np.power(self.reduced_dimensions, 1 / 3)))
         dimensions = [cube_side for dimension in self.original_shape]
         selected_freqs = flat.reshape(dimensions)
-        to_pad = [((dimension-cube_side)//2, (dimension-cube_side)//2) 
+        to_pad = [((dimension - cube_side) // 2, (dimension - cube_side) // 2)
                   for dimension in self.original_shape]
         encoded = np.pad(selected_freqs, to_pad, 'constant')
         return encoded
 
     def encode(self, input):
         """Encode passed image
-        
+
         Parameters
         ----------
         input: numpy array
@@ -189,8 +182,9 @@ class Fourier:
         self.original_shape = input
         mask = self._mask
         # Mask off only ~n frequencies
-        shiftedfftcell = np.fft.fftshift(fftcell) # shift max freq around center
-        fftless = shiftedfftcell*mask # mask
+        shiftedfftcell = np.fft.fftshift(
+            fftcell)  # shift max freq around center
+        fftless = shiftedfftcell * mask  # mask
         return fftless
 
     def decode(self, encoded):
@@ -202,8 +196,9 @@ class Fourier:
 class Autoencoder:
     def __init__(self, train, test):
         pass
+
     def fit(self):
         pass
+
     def err(self, original, recon):
         pass
-    
